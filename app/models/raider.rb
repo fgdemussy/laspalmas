56799a56433ce9db108fe04feb70b33def098d79
Francois <fgdemussy@gmail.com>
2015-08-02 17:35:09 -0300

    basic validations

--- app/models/raider.rb
+++ app/models/raider.rb
@@ -1,2 +1,23 @@
 class Raider < ActiveRecord::Base
+  include ActionView::Helpers::NumberHelper
+  # validates :acceptedTerms, acceptance: true
+  validates :name, :lastName, :rut, presence: true
+  validates :rut, uniqueness: true, rut: true
+
+
+  before_validation :correct_rut_format
+
+  def rut_to_human
+    r = self.rut
+    check_digit = r.last
+    r = r.chop
+    r = r.to_i
+    r = number_with_delimiter(r, :delimiter => ".")
+    r.to_s()+"-"+check_digit
+  end
+
+  private
+  def correct_rut_format
+    self.rut.gsub!(/[-]|[.]|\s/, "") unless rut.nil?
+  end
 end
