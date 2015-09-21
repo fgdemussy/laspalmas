718048fe148a642e99a0945c2c56bfd8b26f93ea
Francois <fgdemussy@gmail.com>
2015-09-21 08:38:32 -0300

    Date from last visit

--- app/helpers/raiders_helper.rb
+++ app/helpers/raiders_helper.rb
@@ -1,2 +1,11 @@
 module RaidersHelper
+
+  def days_ago_or_today(from_date)
+    diff = (Date.today - from_date).round
+    if diff > 1
+      time_ago_in_words(from_date)
+    else
+      return "Today"
+    end
+  end
 end
